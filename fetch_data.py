0f984a52414fafdaf27ed9062398ea17ababe443
Le Tang <letang.boon@gmail.com>
2016-07-31 12:43:32 +0800

    add fetch_data.py, MC mod, run.py

---
+++ fetch_data.py
@@ -0,0 +1,5 @@
+'''
+This module contains your code to fetch text data from 
+the web and removes any non-text data from it (ff the term
+is new to you, we recommend you check out Python modules here).
+'''
---
+++ run.py
@@ -0,0 +1,7 @@
+'''
+Include your code that ties the flow of the application 
+together. You will be starting this Python script on the 
+command line
+'''
+
+from markov_python.cc_markov import MarkovChain
